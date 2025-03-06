1270e989afb32f8031abeb9555a05ee1bebfc3e8
Slawomir Dyk <slawomir.dyk@astrosat.net>
2025-03-06 12:45:18 +0000

    check how a pipeline works

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 import { Flex } from "@radix-ui/themes";
 
 export default function Home() {
-  return (
-    <Flex justify={"center"} >
-      Main Content
-    </Flex>
-  );
+  return <Flex justify={"center"}>Main Content Test</Flex>;
 }
